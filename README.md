0219ecab75b51ce242e8d6817db483edeaaf30a8
dxq613 <dxq613@gmail.com>
2017-09-26 14:06:07 +0800

    fist add

---
+++ README.md
@@ -0,0 +1,11 @@
+# F2: a canvas library which providing 2d draw for mobile
+
+## Installing
+
+
+
+## Contributing
+
+- project structure
+- core style guide
+- work flow
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const {
+  resolve
+} = require('path');
+
+module.exports = {
+  entry: {
+    g: './index.js'
+  },
+  output: {
+    filename: '[name].js',
+    library: 'F2',
+    libraryTarget: 'umd',
+    path: resolve(__dirname, 'build/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            plugins: [
+              'transform-remove-strict-mode'
+            ],
+            presets: [
+              [
+                'es2015', {
+                  loose: true,
+                  modules: false
+                }
+              ],
+              'stage-0'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
+};
