f8a5972ae475f7622f4658dffeead37421a6b376
dxq613 <dxq613@gmail.com>
2017-10-26 10:30:22 +0800

    change read me, add demo

--- core.js
+++ core.js
@@ -1,3 +1,6 @@
 const F2 = {};
 F2.Chart = require('./src/chart/chart');
+F2.Shape = require('./src/geom/shape/shape');
+F2.Graphic = require('./src/graphic/g');
+
 module.exports = F2;
--- test/demos/interval.js
+++ test/demos/interval.js
@@ -289,4 +289,168 @@ describe('interval', () => {
       .adjust('dodge');
     chart.render();
   });
+
+  describe('custom shape', () => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.height = 500;
+    canvas.id = 'interval-tri';
+    div.appendChild(canvas);
+
+    const data = [
+      { tem: 500, city: '3月' },
+      { tem: -50, city: '4月' },
+      { tem: 450, city: '5月' },
+      { tem: -40, city: '6月' },
+      { tem: 690, city: '7月' },
+      { tem: 346, city: '8月' }
+    ];
+    const drawShape = function(points, canvas, cfg) {
+      const ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.moveTo(points[0].x, points[0].y);
+      if (points.length > 1) {
+        for (let i = 1; i <= points.length - 1; i++) {
+          ctx.lineTo(points[i].x, points[i].y);
+        }
+      }
+      ctx.fillStyle = cfg.fill;
+      ctx.fill();
+    };
+    // 自定义绘制数据的的形状
+    const Shape = F2.Shape;
+    Shape.registerShape('interval', 'polygon', {
+      getPoints(cfg) {
+        const x = cfg.x;
+        const y = cfg.y;
+        const y0 = cfg.y0;
+        const width = cfg.size;
+        return [
+          { x: x - width / 2, y: y0 },
+          { x, y },
+          { x: x + width / 2, y: y0 }
+        ];
+      },
+      draw(cfg, canvas) {
+        const points = this.parsePoints(cfg.points);
+        const style = cfg.style || {};
+        style.fill = cfg.color;
+        drawShape(points, canvas, style);
+      }
+    });
+    const chart = new F2.Chart({
+      id: 'interval-tri'
+    });
+    chart.source(data, {
+      tem: {
+        tickCount: 5
+      }
+    });
+    chart.axis('city', {
+      label: {
+        font: 'sans-serif '
+      },
+      line: null,
+      grid: null
+    });
+    chart.axis('tem', {
+      label: null,
+      grid: {
+        stroke: '#f8f8f8'
+      }
+    });
+    chart.interval().position('city*tem')
+      .color('tem*city', function(tem, city) {
+        if (city === '8月') {
+          return '#f5623a';
+        }
+        if (tem >= 0) {
+          return '#f8bdad';
+        }
+        if (tem < 0) {
+          return '#99d6c0';
+        }
+      })
+      .shape('polygon');
+    // 绘制数据
+    data.forEach(function(obj) {
+      const offsetY = obj.tem > 0 ? -14 : 14;
+      chart.guide().html([ obj.city, obj.tem ], `<div style='color: #999999;'><span>${obj.tem}</span></div>`, {
+        align: 'cc',
+        offset: [ 0, offsetY ]
+      });
+    });
+    chart.render();
+  });
+
+  describe('inteval animate', () => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.height = 500;
+    canvas.id = 'interval-animate';
+    div.appendChild(canvas);
+    const data = [
+      { tem: 500, month: '3月' },
+      { tem: -50, month: '4月' },
+      { tem: 450, month: '5月' },
+      { tem: -40, month: '6月' },
+      { tem: 690, month: '7月' },
+      { tem: 346, month: '8月' }
+    ];
+    const chart = new F2.Chart({
+      id: 'interval-animate'
+    });
+    chart.source(data, {
+      tem: {
+        tickCount: 5
+      }
+    });
+    chart.axis('month', {
+      label: {
+        font: 'sans-serif '
+      },
+      line: null,
+      grid: null
+    });
+    chart.axis('tem', {
+      label: null,
+      grid: {
+        stroke: '#f8f8f8'
+      }
+    });
+    chart.interval().position('month*tem').color('tem*month', function(tem, month) {
+      if (month === '8月') {
+        return '#f5623a';
+      }
+      if (tem >= 0) {
+        return '#f8bdad';
+      }
+      if (tem < 0) {
+        return '#99d6c0';
+      }
+    });
+    // y轴方向的缩放动画
+    chart.animate({
+      type: 'scaley'
+    });
+    // 辅助元素
+    data.forEach(function(obj, index) {
+      // 文字部分
+      const offsetY = obj.tem > 0 ? -16 : 14;
+      chart.guide().html([ obj.month, obj.tem ], `<div style='color: #999999;'><span>${obj.tem}</span></div>`, {
+        align: 'cc',
+        offset: [ 0, offsetY ]
+      });
+      // 背景部分
+      const offset = 0.25;
+      chart.guide().rect([ index - offset, 'max' ], [ index + offset, 'min' ], { fill: '#f8f8f8' });
+    });
+    chart.render();
+  });
 });
