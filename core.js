393f99b07a644ae263d5a75f76bf73b42291c495
dxq613 <dxq613@gmail.com>
2017-10-27 11:38:30 +0800

    add global

--- core.js
+++ core.js
@@ -1,4 +1,7 @@
 const F2 = {};
+
+F2.version = '3.0.0-rc1.2';
+F2.Global = require('./src/global');
 F2.Chart = require('./src/chart/chart');
 F2.Shape = require('./src/geom/shape/shape');
 F2.Graphic = require('./src/graphic/g');
