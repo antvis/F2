1a6194cf1a46ed12251577569429de046e65592f
dxq613 <dxq613@gmail.com>
2017-11-02 16:27:23 +0800

    update version

--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 const F2 = {};
 
-F2.version = '3.0.0-rc1.5';
+F2.version = '3.0.0-rc1.7';
 F2.Global = require('./src/global');
 F2.Chart = require('./src/chart/chart');
 F2.Shape = require('./src/geom/shape/shape');
