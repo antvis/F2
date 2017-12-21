19048d3e261daddbd78ee571e1f39239d463f780
再飞 <zaifei.zwx@alibaba-inc.com>
2017-12-21 22:45:43 +0800

    feat: refactor axis component and add top property.

--- src/axis/line.js
+++ src/axis/line.js
@@ -1,48 +1,33 @@
-/**
- * @fileOverview 线性的坐标轴
- * @author dxq613@gmail.com
- */
-
 const Abstract = require('./abstract');
-// const G = require('../graphic/g');
-const Vector2 = require('../graphic/vector2');
+const Vector2 = require('../g/util/vector2');
 const Util = require('../util/common');
 
 class AxisLine extends Abstract {
-
   getDefaultCfg() {
     const cfg = super.getDefaultCfg();
-    cfg.start = null;
-    cfg.end = null;
-    return cfg;
+    return Util.mix({}, cfg, {
+      start: null,
+      end: null
+    });
   }
   // 获取坐标轴上的点
   getOffsetPoint(value) {
-    const self = this;
-    const start = self.get('start');
-    const end = self.get('end');
-    const rangeX = end.x - start.x;
-    const rangeY = end.y - start.y;
+    const { start, end } = this;
     return {
-      x: start.x + rangeX * value,
-      y: start.y + rangeY * value
+      x: start.x + (end.x - start.x) * value,
+      y: start.y + (end.y - start.y) * value
     };
   }
 
   // 获取坐标轴上点的向量，极坐标下覆盖此方法
   getAxisVector() {
-    const self = this;
-    const start = self.get('start');
-    const end = self.get('end');
+    const { start, end } = this;
     return new Vector2(end.x - start.x, end.y - start.y);
   }
 
   drawLine(lineCfg) {
-    const self = this;
-    const container = self.get('container');
-    const start = self.get('start');
-    const end = self.get('end');
-    // G.drawLine(start, end, container, lineCfg);
+    const container = this.getContainer(lineCfg.top);
+    const { start, end } = this;
     container.addShape('line', {
       className: 'axis-line',
       attrs: Util.mix({
