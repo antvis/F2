ac768901be268c84b6e511be93b2b19462c3e0ab
leungwensen <leungwensen@gmail.com>
2018-01-23 14:31:11 +0800

    chore(dev): always exports the latest version

--- src/core.js
+++ src/core.js
@@ -1,6 +1,6 @@
 const F2 = {};
 
-F2.version = '3.0.0';
+F2.version = '____F2_VERSION____';
 F2.Global = require('./global');
 F2.Chart = require('./chart/chart');
 F2.Shape = require('./geom/shape/shape');
