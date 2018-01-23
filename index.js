a45c99d83657d4efe4cda10565aa7141e12b4faf
leungwensen <leungwensen@gmail.com>
2018-01-23 14:25:28 +0800

    chore(dev): standardize npm pkg, provide ES5 version source

--- index.js
+++
@@ -1,8 +0,0 @@
-
-require('./src/animate/');
-require('./src/geom/');
-require('./src/geom/adjust/');
-require('./src/guide/');
-const F2 = require('./core');
-
-module.exports = F2;
---
+++ src/core.js
@@ -0,0 +1,10 @@
+const F2 = {};
+
+F2.version = '3.0.0';
+F2.Global = require('./global');
+F2.Chart = require('./chart/chart');
+F2.Shape = require('./geom/shape/shape');
+F2.Graphic = require('./graphic/g');
+F2.Util = require('./util');
+
+module.exports = F2;
