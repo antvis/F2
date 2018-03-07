8046d43f7e657c9db93c6189f27138416bc7e686
sima.zhang1990@gmail.com <zaifei.zwx@alibaba-inc.com>
2018-03-07 11:26:04 +0800

    animate better

--- src/graphic/animate/animator.js
+++ src/graphic/animate/animator.js
@@ -40,14 +40,13 @@ class Animator {
     this.startTime = 0;
     this.endTime = 0;
     this.time = 0;
-    this.propertyAnims = [];
     this.hasStarted = false;
     this.hasEnded = false;
 
     this.shape = shape;
     this.source = source;
     this.timeline = timeline;
-    this.animGroups = [];
+    this.animGroup = [];
   }
 
   // delay, attrs, duration, easing
@@ -56,57 +55,48 @@ class Animator {
     const attrs = cfg.attrs || {};
     const duration = cfg.duration || 1000;
     const easing = Easing[cfg.easing] || Easing.linear;
-    const animGroup = [];
 
+    const animInfo = {
+      shape: this.shape,
+      startTime: this.timeline.time + delay + this.endTime,
+      endTime: this.timeline.time + delay + this.endTime + duration,
+      easing,
+      parent: this
+    };
+    const interpolate = {}; // 差值函数
     for (const attrName in attrs) {
-      const animInfo = {
-        shape: this.shape, // Shape 对象
-        key: attrName,
-        startValue: this.source[attrName],
-        endValue: attrs[attrName],
-        startTime: this.timeline.time + delay + this.endTime,
-        endTime: this.timeline.time + delay + this.endTime + duration,
-        easing,
-        parent: this
-      };
-
-      let diff;
       let startValue = this.source[attrName];
       let endValue = attrs[attrName];
       if (attrName === 'points') {
         startValue = plainArray(startValue);
         endValue = plainArray(endValue);
-        diff = interpolateArray(startValue, endValue);
-        animInfo.startValue = startValue;
-        animInfo.endValue = endValue;
+        interpolate.points = interpolateArray(startValue, endValue);
+        this.source.points = startValue;
+        attrs.points = endValue;
       } else if (attrName === 'matrix') {
-        diff = interpolateArray(startValue, endValue);
+        interpolate.matrix = interpolateArray(startValue, endValue);
       } else {
-        diff = interpolateNumber(startValue, endValue);
+        interpolate[attrName] = interpolateNumber(startValue, endValue);
       }
-      animInfo.diff = diff;
-      this.timeline.anims.push(animInfo);
-      animGroup.push(animInfo);
     }
-    this.animGroups.push(animGroup);
+    animInfo.interpolate = interpolate;
+    animInfo.startState = this.source;
+    animInfo.endState = attrs;
+
+    this.animGroup.push(animInfo);
+    this.timeline.anims.push(animInfo);
+
     this.endTime += delay + duration;
     return this;
   }
 
   onStart(callback) {
-    const currentAnimGroup = this.animGroups[this.animGroups.length - 1];
-    if (!currentAnimGroup) return;
+    const currentAnim = this.animGroup[this.animGroup.length - 1]; // 获取当前的动画
+    if (!currentAnim) return;
 
-    let called = false;
-
-    currentAnimGroup.forEach(function(anim) {
-      anim.onStart = function() {
-        if (!called) {
-          called = true;
-          callback();
-        }
-      };
-    });
+    currentAnim.onStart = function() {
+      callback();
+    };
 
     return this;
   }
@@ -119,19 +109,12 @@ class Animator {
   }
 
   onEnd(callback) {
-    const currentAnimGroup = this.animGroups[this.animGroups.length - 1];
-    if (!currentAnimGroup) return;
+    const currentAnim = this.animGroup[this.animGroup.length - 1]; // 获取当前的动画
+    if (!currentAnim) return;
 
-    let called = false;
-
-    currentAnimGroup.forEach(function(anim) {
-      anim.onEnd = function() {
-        if (!called) {
-          called = true;
-          callback();
-        }
-      };
-    });
+    currentAnim.onEnd = function() {
+      callback();
+    };
 
     return this;
   }
