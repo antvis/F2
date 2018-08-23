80ba0c13f7d93aa358e572a7cb86f862605255af
simaQ <sima.zhang1990@gmail.com>
2018-08-23 11:48:33 +0800

    clean some demos.

--- demos/fund/js/util.js
+++
@@ -1,9 +0,0 @@
-  // 监听横竖屏
-  window.addEventListener('orientationchange', function() {
-    if (window.orientation === 180 || window.orientation === 0) {
-      window.location.reload();
-    }
-    if (window.orientation === 90 || window.orientation === -90) {
-      window.location.reload();
-    }
-  });
