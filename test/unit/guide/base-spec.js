14f5565ca167d1905db1b766aebd1a978ced7f2a
SimaQ <sima.zhang1990@gmail.com>
2018-09-04 10:29:17 +0800

    Revert "feat: position support for mixing of keyword, percent and value."

--- test/unit/guide/base-spec.js
+++ test/unit/guide/base-spec.js
@@ -31,7 +31,6 @@ describe('Guide Base', function() {
     };
     const guide = new Guide(cfg);
 
-    // data value
     const result = guide.parsePoint(coord, point);
     expect(result.x).to.be.equal(225);
     expect(result.y).to.be.equal(225);
@@ -40,7 +39,7 @@ describe('Guide Base', function() {
     const point1 = [ '50%', '60%' ];
     const result1 = guide.parsePoint(coord, point1);
     expect(result1.x).to.be.equal(225);
-    expect(result1.y).to.be.equal(190);
+    expect(result1.y).to.be.equal(260);
 
     // KEYWORK
     const point2 = [ 'min', 'max' ];
@@ -48,31 +47,13 @@ describe('Guide Base', function() {
     expect(result2.x).to.be.equal(50);
     expect(result2.y).to.be.equal(400);
 
-    // percent + value
-    const point3 = [ '20%', 600 ];
-    const result3 = guide.parsePoint(coord, point3);
-    expect(result3.x).to.be.equal(120);
-    expect(result3.y).to.be.equal(225);
-
-    // keyword + value
-    const point4 = [ 2, 'median' ];
-    const result4 = guide.parsePoint(coord, point4);
-    expect(result4.x).to.be.equal(225);
-    expect(result4.y).to.be.equal(225);
-
-    // keyword + percent
-    const point5 = [ 'min', '60%' ];
-    const result5 = guide.parsePoint(coord, point5);
-    expect(result5.x).to.be.equal(50);
-    expect(result5.y).to.be.equal(190);
-
     // callback
-    const point6 = function(xScale) {
+    const point3 = function(xScale) {
       return [ xScale.values[2], 'max' ];
     };
-    const result6 = guide.parsePoint(coord, point6);
-    expect(result6.x).to.be.equal(225);
-    expect(result6.y).to.be.equal(400);
+    const result3 = guide.parsePoint(coord, point3);
+    expect(result3.x).to.be.equal(225);
+    expect(result3.y).to.be.equal(400);
   });
 
   it('limitInPlot', function() {
