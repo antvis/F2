4f68a868dc67af3136db3730366397d1a84673c0
army8735 <army8735@qq.com>
2019-04-17 14:05:10 +0800

    3.3.9

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ const Theme = require('./theme');
 const Util = require('./util/common');
 
 const Global = {
-  version: '3.3.8',
+  version: '3.3.9',
   scales: {},
   widthRatio: {
     column: 1 / 2,
