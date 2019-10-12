bfe5042207c916e17a1143329fd8cd123a077053
simaQ <sima.zhang1990@gmail.com>
2019-10-12 16:16:23 +0800

    fix: 图例只支持分类数据

---
+++ test/bug/issue-586-spec.js
@@ -0,0 +1,59 @@
+const expect = require('chai').expect;
+
+const F2 = require('../../src/core');
+require('../../src/geom/schema');
+const data = require('../fixtures/mobile-k.json');
+
+const canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 500;
+canvas.id = '586';
+document.body.appendChild(canvas);
+
+describe('issue 586', () => {
+  let canvas;
+  let chart;
+  before(() => {
+    canvas = document.createElement('canvas');
+    canvas.width = 300;
+    canvas.height = 300;
+    canvas.id = 'issue550';
+    document.body.appendChild(canvas);
+  });
+
+  it('issue#586', () => {
+    const throwFn = () => {
+      try {
+        data.sort((obj1, obj2) => {
+          return obj1.time > obj2.time ? 1 : -1;
+        });
+        data.forEach(obj => {
+          obj.range = [ obj.start, obj.end, obj.max, obj.min ];
+          obj.trend = (obj.start <= obj.end) ? 0 : 1;
+        });
+
+        chart = new F2.Chart({
+          id: '586',
+          width: 400,
+          height: 300
+        });
+        chart.source(data);
+        chart.schema().position('time*range')
+          .color('trend', trend => [ '#C00000', '#19B24B' ][trend])
+          .shape('candle');
+        chart.render();
+      } catch (err) {
+        throw new Error(err);
+      }
+    };
+    expect(throwFn).to.not.throw();
+
+    const legendItems = chart.getLegendItems();
+    expect(legendItems).to.be.empty;
+  });
+
+  after(() => {
+    chart.destroy();
+    document.body.removeChild(canvas);
+  });
+});
