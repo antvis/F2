da0b6fe7db187554edf6977ddeb8b9290da01779
索丘 <yezengyue@gmail.com>
2020-01-07 10:04:42 +0800

    chore: 发布3.5.0-beta.6

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ const Theme = require('./theme');
 const Util = require('./util/common');
 
 const Global = {
-  version: '3.5.0-beta.5',
+  version: '3.5.0-beta.6',
   scales: {},
   widthRatio: {
     column: 1 / 2,
