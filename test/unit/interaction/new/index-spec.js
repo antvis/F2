c2f0f67885447ed7db1db585c52c9219ff8f0766
索丘 <yezengyue@gmail.com>
2020-04-23 15:51:57 +0800

    test: add onstart hooks test case

--- test/unit/interaction/new/index-spec.js
+++ test/unit/interaction/new/index-spec.js
@@ -1,5 +1,8 @@
-const F2 = require('../../../../src/index');
 import data from './data.json';
+
+const F2 = require('../../../../src/index');
+
+const onStartCallback = jest.fn();
 
 const canvas = document.createElement('canvas');
 canvas.width = 500;
@@ -27,15 +30,20 @@ chart.line()
   .position('reportDate*rate')
   .color('name');
 
-chart.interaction('pinch');
+chart.interaction('pinch', {
+  onStart: onStartCallback
+});
 chart.interaction('pan');
 chart.render();
 
 describe('Interaction', () => {
   it('pinch', () => {
-    const interactionContext = chart.get('interactionContext');
-    interactionContext.start();
-    interactionContext.doZoom(0.5, 0.5, 1.5);
+    // const interactionContext = chart.get('interactionContext');
+    // interactionContext.start();
+    // interactionContext.doZoom(0.5, 0.5, 1.5);
+
+    chart.get('canvas').emit('pinchstart', {});
+    expect(onStartCallback.mock.calls.length).toBe(1);
   });
 
   it('pan', () => {
