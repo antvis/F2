b5f47fa535050bdca2456ed3df58cf15aa81a331
edokeh <edokeh@163.com>
2020-06-09 22:54:45 +0800

    test: 补充新 pan 方法的单测

--- test/unit/interaction/new/index-spec.js
+++ test/unit/interaction/new/index-spec.js
@@ -1,5 +1,5 @@
 import data from './data.json';
-
+const expect = require('chai').expect;
 const F2 = require('../../../../src/index');
 
 const onStartCallback = jest.fn();
@@ -38,17 +38,27 @@ chart.render();
 
 describe('Interaction', () => {
   it('pinch', () => {
-    // const interactionContext = chart.get('interactionContext');
-    // interactionContext.start();
-    // interactionContext.doZoom(0.5, 0.5, 1.5);
+    const interactionContext = chart.get('interactionContext');
+    interactionContext.start();
+    interactionContext.doZoom(0.5, 0.5, 1.5);
 
     chart.get('canvas').emit('pinchstart', {});
-    expect(onStartCallback.mock.calls.length).toBe(1);
+    expect(onStartCallback.mock.calls.length).to.eql(1);
   });
 
   it('pan', () => {
+    const beforePoints = chart.get('canvas').get('children')[1].get('children')[0].get('children')[0].get('attrs').points;
+    const firstPoint = beforePoints.find(p => !isNaN(p.x));
+    expect(firstPoint.reportDate).to.eql('2017-03-15');
+    expect(firstPoint.rate).to.eql(-1.3);
+
     const interactionContext = chart.get('interactionContext');
     interactionContext.start();
     interactionContext.doMove(0.1);
+    const afterPoints = chart.get('canvas').get('children')[1].get('children')[0].get('children')[0].get('attrs').points;
+    const afterPoint = afterPoints.find(p => !isNaN(p.x));
+    expect(afterPoint.reportDate).to.eql('2017-02-15');
+    expect(afterPoint.rate).to.eql(-6.9);
+
   });
 });
