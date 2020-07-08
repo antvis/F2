33a73d5b8d596cb323e65c837e8f5cb06964fbba
索丘 <yezengyue@gmail.com>
2020-07-08 21:56:52 +0800

    chore: release 3.7.0-alpha.6

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ import Theme from './theme';
 import { deepMix } from './util/common';
 
 const Global = {
-  version: '3.7.0-alpha.5',
+  version: '3.7.0-alpha.6',
   scales: {},
   widthRatio: {
     column: 1 / 2,
