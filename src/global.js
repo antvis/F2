cfd944193270c96302206006a5678a065731df27
索丘 <yezengyue@gmail.com>
2020-07-10 00:30:15 +0800

    chore: release 3.7.0

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ import Theme from './theme';
 import { deepMix } from './util/common';
 
 const Global = {
-  version: '3.7.0-alpha.7',
+  version: '3.7.0',
   scales: {},
   widthRatio: {
     column: 1 / 2,
