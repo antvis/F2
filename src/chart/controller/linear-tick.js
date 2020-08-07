b0594f64bd51856518b39f8774f315840079525b
cyckoo <cyckoo@qq.com>
2020-08-07 11:54:08 +0800

    feat: 刻度值优选原则

--- src/chart/controller/linear-tick.js
+++ src/chart/controller/linear-tick.js
@@ -1,94 +1,135 @@
 
-import { fixedBase } from '@antv/util';
-import { isNil } from '../../util/common';
-import AutoUtil from './scale-util';
-const { snapFactorTo, snapMultiple } = AutoUtil;
+const DDEFAULT_COUNT = 5;
 
-// 默认经验刻度
-const SNAP_COUNT_ARRAY = [ 1, 1.2, 1.5, 1.6, 2, 2.2, 2.4, 2.5, 3, 4, 5, 6, 7.5, 8, 10 ];
-const DEFAULT_TICK_COUNT = 5;
-const EPS = 1e-12;
+export default cfg => {
+  const { min, max, tickCount } = cfg || {};
+  const count = tickCount || DDEFAULT_COUNT;
+  // 1.计算平均刻度间隔
+  const avgInterval = (max - min) / (count - 1);
 
-// linear连续数值轴刻度nice算法
-export default info => {
+  // 2.转化成1~10的刻度间隔值
+  const factor = getFactor(avgInterval);
+  // 3.获取满足tickCount的情况下，最优刻度值
+  const interval = getBestInterval({ tickCount: count, avgInterval, max, min, factor });
+  const minTickPosition = Math.ceil(Math.abs(min / interval));
+  const minTick = min > 0 ? minTickPosition * interval : -minTickPosition * interval;
 
-  let { min, max, interval, tickCount = DEFAULT_TICK_COUNT } = info;
-  // 用户传入的经验刻度
-  const snapArray = info.snapArray || SNAP_COUNT_ARRAY;
-
-  if (Math.abs(max - min) < EPS) {
-    if (min === 0) {
-      max = 1;
-    } else {
-      if (min > 0) {
-        min = 0;
-      } else {
-        max = 0;
-      }
-    }
-    if (max - min < 5 && !interval && max - min >= 1) {
-      interval = 1;
-    }
-  }
-
-  if (isNil(interval)) {
-    // 计算间距
-    const temp = (max - min) / (tickCount - 1);
-    interval = snapFactorTo(temp, snapArray, 'ceil');
-  }
-
-  // 如果有自定义间隔
-  if (info.interval) {
-    // 校正 max 和 min
-    max = snapMultiple(max, interval, 'ceil'); // 向上逼近
-    min = snapMultiple(min, interval, 'floor'); // 向下逼近
-
-    tickCount = Math.round((max - min) / interval);
-    min = fixedBase(min, interval); // 当min为负数的时候，fixedBase后，min可能会大于minLimit，导致最终产出的tick是大于minLimit的，所以必须进行修正
-    max = fixedBase(max, interval);
-  } else {
-    const avg = (max + min) / 2;
-    const avgTick = snapMultiple(avg, interval, 'ceil');
-    const sideCount = Math.floor((tickCount - 2) / 2);
-    let maxTick = fixedBase(avgTick + sideCount * interval, interval);
-    let minTick;
-    if (tickCount % 2 === 0) {
-      minTick = fixedBase(avgTick - sideCount * interval, interval);
-    } else {
-      minTick = fixedBase(avgTick - (sideCount + 1) * interval, interval);
-    }
-
-    let prevMaxTick = null;
-    // 顶部tick矫正
-    // 如果减去intervl, fixBase后，新的minTick没有大于之前的值，就退出，防止死循环
-    while (maxTick < max && (prevMaxTick === null || maxTick > prevMaxTick)) { // 保证计算出来的刻度最大值 maxTick 不小于数据最大值 max
-      prevMaxTick = maxTick;
-      maxTick = fixedBase(maxTick + interval, interval);
-    }
-
-    let prevMinTick = null;
-    // 底部tick矫正
-    // 如果减去intervl, fixBase后，新的minTick没有小于之前的值，就退出，防止死循环
-    while (minTick > min && (prevMinTick === null || minTick < prevMinTick)) { // 保证计算出来的刻度最小值 minTick 不大于数据最小值 min
-      prevMinTick = minTick;
-      minTick = fixedBase(minTick - interval, interval); // 防止超常浮点数计算问题
-    }
-    max = maxTick;
-    min = minTick;
-  }
+  let tickLength = 0;
 
   const ticks = [];
-
-  ticks.push(min);
-  for (let i = 1; i < tickCount; i++) {
-    const tickValue = fixedBase(interval * i + min, interval);
-    if (tickValue < max) {
-      ticks.push(tickValue);
-    }
+  while (minTick + tickLength * interval < max) {
+    ticks.push(minTick + tickLength * interval);
+    tickLength++;
   }
-  if (ticks[ticks.length - 1] < max) {
-    ticks.push(max);
-  }
-
+  ticks.push(minTick + tickLength * interval);
   return ticks;
 };
+
+const DECIMAL_LENGTH = 12;
+function getFactor(number) {
+  if (number === Infinity || number === -Infinity) {
+    throw new Error('Not support Infinity!');
+  }
+
+  // 取正数
+  number = Math.abs(number);
+  let factor = 1;
+
+  // 小于1,逐渐放大
+  if (number < 1) {
+    let count = 0;
+    while (number < 1) {
+      factor = factor / 10;
+      number = number * 10;
+      count++;
+    }
+    // 浮点数计算出现问题
+    if (factor.toString().length > DECIMAL_LENGTH) {
+      factor = parseFloat(factor.toFixed(count));
+    }
+    return factor;
+  }
+
+  // 大于10逐渐缩小
+  while (number > 10) {
+    factor = factor * 10;
+    number = number / 10;
+  }
+
+  return factor;
+}
+
+// 获取最佳匹配刻度
+function getBestInterval({ tickCount, avgInterval, max, min, factor }) {
+  const SNAP_COUNT_ARRAY = [ 1, 1.2, 1.5, 1.6, 2, 2.2, 2.4, 2.5, 3, 4, 5, 6, 7.5, 8, 10 ];
+  const calInterval = avgInterval / factor;
+  const calMax = max / factor;
+  const calMin = min / factor;
+
+  // 相似数
+  let similarityInterval;
+  let similarityIndex;
+
+  for (let index = 0; index < SNAP_COUNT_ARRAY.length; index++) {
+    const item = SNAP_COUNT_ARRAY[index];
+    const nextItem = SNAP_COUNT_ARRAY[index + 1];
+    if (index === 0 && item <= calInterval) {
+      similarityInterval = item;
+      similarityIndex = 0;
+    }
+
+    // last
+    if (index === SNAP_COUNT_ARRAY.length - 1 && item <= calInterval) {
+      similarityInterval = item;
+      similarityIndex = index;
+    }
+
+    if (index <= SNAP_COUNT_ARRAY.length - 2) {
+      if (calInterval >= item && calInterval < nextItem) {
+        // 取更加逼近的刻度
+        if (Math.abs(calInterval - item) > Math.abs(calInterval - nextItem)) {
+          similarityInterval = nextItem;
+          similarityIndex = index + 1;
+        } else {
+          similarityInterval = item;
+          similarityIndex = index;
+        }
+      }
+    }
+  }
+
+  if (intervalIsVerify({ interval: similarityInterval, tickCount, max: calMax, min: calMin })) {
+    return similarityInterval * factor;
+  }
+
+  // 最后一个接直接返回
+  if (similarityIndex === SNAP_COUNT_ARRAY.length - 1) {
+    return similarityInterval * factor;
+  }
+
+  similarityIndex++;
+  while (similarityIndex < SNAP_COUNT_ARRAY.length) {
+    if (intervalIsVerify({ interval: SNAP_COUNT_ARRAY[similarityIndex], tickCount, max: calMax, min: calMin })) {
+      similarityInterval = SNAP_COUNT_ARRAY[similarityIndex];
+      break;
+    }
+    similarityIndex++;
+  }
+  return similarityInterval * factor;
+}
+
+// 刻度是否满足展示需求
+function intervalIsVerify({ interval, tickCount, max, min }) {
+  const maxRange = max - min;
+
+  // 上下要预留间距
+  const maxTickPosition = Math.abs(max) % interval > 0 ? 1 : 0;
+  const minTickPosition = Math.abs(min) % interval > 0 ? 1 : 0;
+  const space = (maxTickPosition + minTickPosition) * interval;
+
+  if (interval * tickCount - maxRange - space >= 0) {
+    return true;
+  }
+
+  return false;
+}
