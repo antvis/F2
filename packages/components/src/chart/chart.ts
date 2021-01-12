16316514add051a16934e9d187788186397fe623
zengyue <yezengyue@gmail.com>
2021-01-12 11:37:16 +0800

    fix: 去掉动画

--- packages/components/src/chart/chart.ts
+++ packages/components/src/chart/chart.ts
@@ -14,6 +14,7 @@ class Chart {
       pixelRatio,
       width,
       height,
+      animate: false,
     });
     this.chart = chart;
 
--- packages/fund-components/test/index.test.tsx
+++ packages/fund-components/test/index.test.tsx
@@ -1,5 +1,7 @@
 // @ts-nocheck
 import data from '../../react/test/data';
+import Chart from '@ali/f2-components';
+import { WeaverLine } from '../src/index'
 
 const canvasEl = document.createElement('canvas');
 canvasEl.style.width = '100%';
@@ -9,7 +11,11 @@ const context = canvasEl.getContext('2d');
 
 
 const App = () => {
-  return null;
+  return (
+    <Chart data={ data } context={ context }>
+      <WeaverLine position="reportDateTimestamp*rate"/>
+    </Chart>
+  );
 }
 
 const chart = App({ data, context });
