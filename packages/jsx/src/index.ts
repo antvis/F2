8e023f8c6d383b16a66d1bd63b983269fa3faab9
zengyue <yezengyue@gmail.com>
2021-01-19 14:53:31 +0800

    types: 添加types定义输出

--- packages/jsx/src/index.ts
+++ packages/jsx/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference path="../typings.d.ts" />
+
 import render from './render';
 import jsx from './jsx-classic';
 
--- packages/jsx/typings.d.ts
+++ packages/jsx/typings.d.ts
@@ -1,1 +1,21 @@
 import '../../typings';
+
+declare namespace JSX {
+  interface Element {
+    type: string;
+    props: any;
+    style: any;
+    children: any;
+    key: string;
+    [propName: string]: any;
+  }
+  interface ElementClass {
+    render(): any;
+  }
+  interface IntrinsicElements {
+    group: any;
+    rect: any;
+    circle: any;
+    text: any;
+  }
+}
