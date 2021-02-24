18af273b9025060983f7644031ed148546f01e85
zengyue <yezengyue@gmail.com>
2021-02-24 14:39:21 +0800

    fix: 修复单测报错

--- packages/fund-components/test/util.ts
+++ packages/fund-components/test/util.ts
@@ -1,6 +1,6 @@
 const createContext = () => {
   const canvasEl = document.createElement('canvas');
-  canvasEl.style.width = '100%';
+  canvasEl.style.width = '359px';
   canvasEl.style.height = '400px';
   document.body.appendChild(canvasEl);
   const context = canvasEl.getContext('2d');
--- packages/jsx/test/render.test.tsx
+++ packages/jsx/test/render.test.tsx
@@ -5,7 +5,7 @@ import { render, jsx } from '../src';
 const { G } = F2;
 
 const canvasEl = document.createElement('canvas');
-canvasEl.style.width = '100%';
+canvasEl.style.width = '359px';
 canvasEl.style.height = '400px';
 document.body.appendChild(canvasEl);
 const context = canvasEl.getContext('2d');
