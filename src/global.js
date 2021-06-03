f656a186a3f716e82de24dcbefc2fec8409d0879
索丘 <yezengyue@gmail.com>
2021-06-03 22:14:23 +0800

    chore: release 3.8.9

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@ import lang from './aria-lang.CN';
 import { deepMix } from './util/common';
 
 const Global = {
-  version: '3.8.8',
+  version: '3.8.9',
   scales: {},
   widthRatio: {
     column: 1 / 2,
