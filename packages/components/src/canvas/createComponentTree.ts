428672eb6017a343093536dbae09a74b3f88110c
zengyue <yezengyue@gmail.com>
2021-06-08 18:12:06 +0800

    fix: 修复react场景下，jsx element 存在react特定对象导致equal死循环的问题

--- packages/components/src/canvas/createComponentTree.ts
+++ packages/components/src/canvas/createComponentTree.ts
@@ -4,18 +4,21 @@ import Container from '../component/container';
 function createComponentTree(element: JSX.Element, appendProps) {
   if (!element) return element;
   const { type, key, ref, props } = element;
-  const { children } = props;
+
+  const children = map(props.children, (child: JSX.Element) => {
+    return createComponentTree(child, appendProps);
+  });
 
   // 如果有children， 统一处理成 Container
-  if (type === 'fragment' && children) {
-    props.children = map(children, (child: JSX.Element) => {
-      return createComponentTree(child, appendProps);
-    });
+  if (type === 'fragment') {
     return {
       type: Container,
       key,
       ref,
-      props,
+      props: {
+        ...props,
+        children,
+      },
     }
   }
   // 其他标签都不处理
@@ -24,7 +27,16 @@ function createComponentTree(element: JSX.Element, appendProps) {
   }
   // 如果已经是F2 component了，直接返回
   if (type.prototype && type.prototype.isF2Component) {
-    return element;
+    // 只保留需要的属性
+    return {
+      type,
+      key,
+      ref,
+      props: {
+        ...props,
+        children,
+      },
+    };
   }
   // 其他情况 说明是 function component, 直接执行
   const newElement = type({
