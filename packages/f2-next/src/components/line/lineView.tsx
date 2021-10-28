afffb35d6330aa90df8a3a2671a09f9f078e3312
zengyue <yezengyue@gmail.com>
2021-10-28 15:48:14 +0800

    feat: 添加竞速排序线图demo

--- packages/f2-next/src/components/line/lineView.tsx
+++ packages/f2-next/src/components/line/lineView.tsx
@@ -1,7 +1,56 @@
 import { jsx } from '../../jsx';
 
+function concat(dataArray) {
+  let result = [];
+  for (let i = 0; i < dataArray.length; i++) {
+    const item = dataArray[i];
+    result = result.concat(item);
+  }
+  return result;
+}
+
+function getPoint(points, t: number) {
+  const firstPoint = points[0];
+  const lastPoint = points[points.length - 1];
+  const xOffset = lastPoint.x - firstPoint.x;
+  const x = firstPoint.x + xOffset * t;
+
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    const prevPoint = points[i - 1];
+    if (x >= prevPoint.x && x <= point.x) {
+      // x 在 2 点之间的比例，根据比例再算出 y 的值
+      const ratio = (x - prevPoint.x) / (point.x - prevPoint.x);
+      return {
+        x,
+        y: prevPoint.y + (point.y - prevPoint.y) * ratio,
+      };
+    }
+  }
+}
+
 export default (props: any) => {
-  const { mappedArray } = props;
+  const { mappedArray, coord, animation, endView: EndView } = props;
+  const { left, top, width, height } = coord;
+
+  const appear = {
+    easing: 'linear',
+    duration: 450,
+    clip: {
+      type: 'Rect',
+      property: ['width'],
+      start: {
+        x: left,
+        y: top,
+        height: height,
+        width: 0,
+      },
+      end: {
+        width: width,
+      },
+    },
+    ...animation,
+  };
   return (
     <group>
       {mappedArray.map((item) => {
@@ -12,12 +61,36 @@ export default (props: any) => {
               <polyline
                 attrs={{
                   points: data,
-                  stroke:  color,
-                  lineWidth:size,
-                  ...shape
+                  stroke: color,
+                  lineWidth: size,
+                  ...shape,
+                }}
+                animation={{
+                  appear,
                 }}
               />
             ))}
+            {EndView ? (
+              <group
+                animation={{
+                  appear: {
+                    easing: appear.easing,
+                    duration: appear.duration,
+                    onFrame: function (t) {
+                      // 这段逻辑有点恶心。。
+                      const { element } = this;
+                      const children = element.get('children');
+                      const point = getPoint(concat(dataArray), t);
+                      children.forEach((child) => {
+                        child.moveTo(point.x, point.y);
+                      });
+                    },
+                  },
+                }}
+              >
+                <EndView {...item} />
+              </group>
+            ) : null}
           </group>
         );
       })}
