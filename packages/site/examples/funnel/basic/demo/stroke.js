ad98d2635027cd847414da7d89680e17367919d4
buli <junyu.junyujiang@alibaba-inc.com>
2021-11-08 20:28:23 +0800

    docs: add funnel chart demo

--- packages/site/examples/funnel/basic/demo/stroke.js
+++ packages/site/examples/funnel/basic/demo/stroke.js
@@ -1,4 +1,6 @@
-import F2 from '@antv/f2';
+import { jsx, Canvas, Chart, Interval, Legend } from '@antv/f2';
+
+const context = document.getElementById('container').getContext('2d');
 
 const data = [
   { action: '浏览网站', pv: 50000, percent: 1 },
@@ -8,43 +10,40 @@ const data = [
   { action: '完成交易', pv: 8000, percent: 0.16 }
 ];
 
-const chart = new F2.Chart({
-  id: 'container',
-  pixelRatio: window.devicePixelRatio,
-  padding: [ 60, 80, 15, 15 ]
-});
+const { props } = (
+  <Canvas context={context} pixelRatio={window.devicePixelRatio}>
+    <Chart
+      data={data}
+      coord={{
+        transposed: true,
+      }}
+      scale={{
+        percent: {
+          min: 0,
+        },
+        action: {
+          range: [0, 1],
+        },
+      }}
+    >
+      <Interval
+        x="action"
+        y="percent"
+        adjust="symmetric"
+        shape="funnel"
+        color={{
+          field: 'action',
+          range: ['#0050B3', '#1890FF', '#40A9FF', '#69C0FF', '#BAE7FF'],
+        }}
+        style={{
+          stroke: '#fff',
+          lineWidth: 2,
+        }}
+      />
+      <Legend />
+    </Chart>
+  </Canvas>
+);
 
-chart.source(data);
-chart.axis(false);
-chart.coord({
-  transposed: true,
-  scale: [ 1, -1 ]
-});
-chart.legend(true);
-chart.intervalLabel({
-  offsetX: 10,
-  label: (data, color) => {
-    return {
-      text: data.action,
-      fill: color
-    };
-  },
-  guide: data => {
-    return {
-      text: (data.percent * 100).toFixed(0) + '%',
-      fill: '#fff'
-    };
-  }
-});
-
-
-chart.interval()
-  .position('action*percent')
-  .color('action', [ '#0050B3', '#1890FF', '#40A9FF', '#69C0FF', '#BAE7FF' ])
-  .adjust('symmetric')
-  .style({
-    lineWidth: 2,
-    stroke: '#fff'
-  })
-  .shape('funnel');
-chart.render();
+const canvas = new Canvas(props);
+canvas.render();
