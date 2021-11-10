4ce06bb6121b70548a2b0a1ad9dd594d9d3d20c2
edokeh <edokeh@163.com>
2021-11-10 22:41:58 +0800

    feat: 补充 line 类型定义

--- packages/f2/src/components/geometry/interface.ts
+++ packages/f2/src/components/geometry/interface.ts
@@ -1,3 +1,6 @@
+import Chart from '../../chart';
+import Coord from '../../coord';
+
 export interface AttrRange {
   shape?: any[];
   color?: any[];
@@ -5,3 +8,20 @@ export interface AttrRange {
 }
 
 export type GeomType = 'line' | 'point' | 'area' | 'polygon' | 'schema' | 'interval';
+
+interface Style {
+  field: string;
+  smooth?: boolean;
+  stroke?: string | ((t) => string);
+  lineWidth?: number | ((t) => number);
+}
+
+export interface GeometryProps {
+  data: any;
+  adjust: any;
+  chart: Chart;
+  coord: Coord;
+  startOnZero: boolean;
+  style: Style;
+  [k: string]: any;  // TODO
+}
