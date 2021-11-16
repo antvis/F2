137574c559318c188aa4547a5d0c2fab954056db
zengyue <yezengyue@gmail.com>
2021-11-16 21:48:18 +0800

    docs: 修改教程里面3.x的代码，并去掉api导航栏

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
       resolve: '@antv/gatsby-theme-antv',
       options: {
         // eslint-disable-next-line quotes
-        GATrackingId: `UA-148148901-6`
-      }
-    }
+        GATrackingId: `UA-148148901-6`,
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
@@ -16,54 +16,58 @@ module.exports = {
     description: 'The Grammar of Graphics in JavaScript',
     siteUrl: 'https://f2-next.antv.vision',
     githubUrl: repository.url,
+    versions: {
+      '3.8.x': 'https://f2.antv.vision',
+      '4.0.x': 'https://f2-next.antv.vision',
+    },
     navs: [
       {
         slug: 'docs/tutorial/getting-started',
         title: {
           zh: '使用文档',
-          en: 'Manual'
-        }
+          en: 'Manual',
+        },
       },
-      {
-        slug: 'docs/api',
-        title: {
-          zh: '组件文档',
-          en: 'Component Documentation'
-        }
-      },
+      // {
+      //   slug: 'docs/api',
+      //   title: {
+      //     zh: '组件文档',
+      //     en: 'Component Documentation'
+      //   }
+      // },
       {
         slug: 'examples/gallery',
         title: {
           zh: '图表示例',
-          en: 'Gallery'
-        }
-      }
+          en: 'Gallery',
+        },
+      },
     ],
     docs: [
       {
         slug: 'tutorial/manual',
         title: {
           zh: '教程',
-          en: 'Tutorial'
+          en: 'Tutorial',
         },
-        order: 1
+        order: 1,
       },
       {
         slug: 'api/chart',
         title: {
           zh: '图表',
-          en: 'Chart'
+          en: 'Chart',
         },
-        order: 1
+        order: 1,
       },
       {
         slug: 'api/graphic',
         title: {
           zh: '绘图',
-          en: 'Graphic'
+          en: 'Graphic',
         },
-        order: 2
-      }
+        order: 2,
+      },
     ],
     examples: [
       {
@@ -71,121 +75,121 @@ module.exports = {
         icon: 'gallery',
         title: {
           zh: '',
-          en: ''
-        }
+          en: '',
+        },
       },
       {
         slug: 'creative',
         icon: 'gallery',
         title: {
           zh: '场景案例',
-          en: 'Creative Charts'
-        }
+          en: 'Creative Charts',
+        },
       },
       {
         slug: 'line',
         icon: 'line',
         title: {
           zh: '折线图',
-          en: 'Line Charts'
-        }
+          en: 'Line Charts',
+        },
       },
       {
         slug: 'area',
         icon: 'area',
         title: {
           zh: '面积图',
-          en: 'Area Charts'
-        }
+          en: 'Area Charts',
+        },
       },
       {
         slug: 'column',
         icon: 'column',
         title: {
           zh: '柱状图',
-          en: 'Column Charts'
-        }
+          en: 'Column Charts',
+        },
       },
       {
         slug: 'bar',
         icon: 'bar',
         title: {
           zh: '条形图',
-          en: 'Bar Charts'
-        }
+          en: 'Bar Charts',
+        },
       },
       {
         slug: 'pie',
         icon: 'pie',
         title: {
           zh: '饼图',
-          en: 'Pie Charts'
-        }
+          en: 'Pie Charts',
+        },
       },
       {
         slug: 'radar',
         icon: 'radar',
         title: {
           zh: '雷达图',
-          en: 'Radar Charts'
-        }
+          en: 'Radar Charts',
+        },
       },
       {
         slug: 'point',
         icon: 'point',
         title: {
           zh: '点图',
-          en: 'Point Charts'
-        }
+          en: 'Point Charts',
+        },
       },
       {
         slug: 'funnel',
         icon: 'funnel',
         title: {
           zh: '漏斗图',
-          en: 'Funnel Charts'
-        }
+          en: 'Funnel Charts',
+        },
       },
       {
         slug: 'candlestick',
         icon: 'candlestick',
         title: {
           zh: '蜡烛图',
-          en: 'Candlestick Charts'
-        }
+          en: 'Candlestick Charts',
+        },
       },
       {
         slug: 'relation',
         icon: 'relation',
         title: {
           zh: '关系图',
-          en: 'Relation Charts'
-        }
+          en: 'Relation Charts',
+        },
       },
       {
         slug: 'heatmap',
         icon: 'heatmap',
         title: {
           zh: '热力图',
-          en: 'Heatmap Charts'
-        }
+          en: 'Heatmap Charts',
+        },
       },
       {
         slug: 'component',
         icon: 'component',
         title: {
           zh: '功能组件',
-          en: 'Chart Components'
-        }
+          en: 'Chart Components',
+        },
       },
       {
         slug: 'other',
         icon: 'other',
         title: {
           zh: '其他图表',
-          en: 'Other Chart'
-        }
-      }
+          en: 'Other Chart',
+        },
+      },
     ],
     playground: {
       playgroundDidMount: 'window.initPlayground()',
@@ -199,11 +203,11 @@ module.exports = {
             <canvas id="container"/>
           </div>
         </div>
-      `
+      `,
     },
     docsearchOptions: {
       apiKey: '349619374e9488b3f000714317d67381',
-      indexName: 'antv_f2'
-    }
-  }
+      indexName: 'antv_f2',
+    },
+  },
 };
