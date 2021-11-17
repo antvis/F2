04aa87a48a811c4e0a217e329f71ce761dfd8fb1
edokeh <edokeh@163.com>
2021-11-17 15:58:38 +0800

    fix: ci

--- packages/f2/src/jsx/interface.d.ts
+++ packages/f2/src/jsx/interface.d.ts
@@ -44,11 +44,11 @@ interface IntrinsicElementsProps {
 }
 
 interface CircleProps extends IntrinsicElementsProps {
-  attrs: SupportPx<CircleAttrs>; 
+  attrs: Partial<SupportPx<CircleAttrs>>;
 }
 
 interface RectProps extends IntrinsicElementsProps {
-  attrs: SupportPx<RectAttrs>;
+  attrs: Partial<SupportPx<RectAttrs>>;
 }
 
 interface Ref {
