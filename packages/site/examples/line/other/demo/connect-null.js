d2bdaebe08e7b8c0883a7e9f858c9363bffc221f
buli <junyu.junyujiang@alibaba-inc.com>
2021-11-18 11:23:17 +0800

    feat: 调整demo

--- packages/site/examples/line/other/demo/connect-null.js
+++ packages/site/examples/line/other/demo/connect-null.js
@@ -1,57 +1,58 @@
-import F2 from '@antv/f2';
+import { jsx, Canvas, Chart, Line, Point, Axis, Tooltip } from '@antv/f2';
 
-const data = [{
-  day: '周一',
-  value: 300
-}, {
-  day: '周二',
-  value: 400
-}, {
-  day: '周三',
-  value: null
-}, {
-  day: '周四',
-  value: 500
-}, {
-  day: '周五',
-  value: 490
-}, {
-  day: '周六',
-  value: 600
-}, {
-  day: '周日',
-  value: 900
-}];
-const chart = new F2.Chart({
-  id: 'container',
-  pixelRatio: window.devicePixelRatio
-});
-
-chart.source(data, {
-  value: {
-    tickCount: 5,
-    min: 0
+const data = [
+  {
+    day: '周一',
+    value: 300,
   },
-  day: {
-    range: [ 0, 1 ]
-  }
-});
-chart.axis('day', {
-  label: function label(text, index, total) {
-    const textCfg = {};
-    if (index === 0) {
-      textCfg.textAlign = 'left';
-    } else if (index === total - 1) {
-      textCfg.textAlign = 'right';
-    }
-    return textCfg;
-  }
-});
-chart.tooltip({
-  showCrosshairs: true
-});
-chart.line({
-  connectNulls: true // 配置，连接空值数据
-}).position('day*value');
-chart.point().position('day*value');
+  {
+    day: '周二',
+    value: 400,
+  },
+  {
+    day: '周三',
+    value: null,
+  },
+  {
+    day: '周四',
+    value: 500,
+  },
+  {
+    day: '周五',
+    value: 490,
+  },
+  {
+    day: '周六',
+    value: 600,
+  },
+  {
+    day: '周日',
+    value: 900,
+  },
+];
+const context = document.getElementById('container').getContext('2d');
+const { props } = (
+  <Canvas context={context} pixelRatio={window.devicePixelRatio}>
+    <Chart
+      data={data}
+      scale={{
+        value: {
+          tickCount: 5,
+          min: 0,
+        },
+        day: {
+          range: [0, 1],
+        },
+      }}
+    >
+      <Axis field="day" />
+      <Axis field="value" />
+      <Line x="day" y="value" connectNulls />
+      <Point x="day" y="value" />
+      <Tooltip showCrosshairs />
+    </Chart>
+  </Canvas>
+);
+
+const chart = new Canvas(props);
 chart.render();
