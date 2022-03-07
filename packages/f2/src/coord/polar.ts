c3a0a045f199afd0e49c21a994a4dd7c8751bba8
Acery <acery.ycy@antgroup.com>
2022-03-07 10:28:50 +0800

    feat: getSnapshot支持饼图 (#1380)

--- packages/f2/src/coord/polar.ts
+++ packages/f2/src/coord/polar.ts
@@ -1,5 +1,6 @@
 import Base from './base';
 import { Range, Option } from './types';
+import { Vector2, Matrix } from '@antv/f2-graphic';
 
 interface PolarOption extends Option {
   radius: number; // 内半径比例
@@ -64,6 +65,43 @@ class Polar extends Base {
       y: center.y + Math.sin(angle) * radius,
     };
   }
+
+  invertPoint(point) {
+    const { center, transposed, x: rangeX, y: rangeY } = this;
+    const x = { start: rangeX[0], end: rangeX[1] }
+    const y = { start: rangeY[0], end: rangeY[1] }
+    const xDim = transposed ? 'y' : 'x';
+    const yDim = transposed ? 'x' : 'y';
+
+    const m = [1, 0, 0, 1, 0, 0];
+    Matrix.rotate(m, m, x.start);
+
+    let startV = [1, 0];
+    Vector2.transformMat2d(startV, startV, m);
+    startV = [startV[0], startV[1]];
+
+    const pointV = [point.x - center.x, point.y - center.y];
+    if (Vector2.zero(pointV)) {
+      return {
+        x: 0,
+        y: 0
+      };
+    }
+
+    let theta = Vector2.angleTo(startV, pointV, x.end < x.start);
+    if (Math.abs(theta - Math.PI * 2) < 0.001) {
+      theta = 0;
+    }
+    const l = Vector2.length(pointV);
+    let percentX = theta / (x.end - x.start);
+    percentX = x.end - x.start > 0 ? percentX : -percentX;
+    const percentY = (l - y.start) / (y.end - y.start);
+    const rst = {};
+    rst[xDim] = percentX;
+    rst[yDim] = percentY;
+    return rst;
+  }
+
 }
 
 export default Polar;
