d1de6d2d7d77b90a3a09fd7bb1ac100a1959d301
zengyue ye <yezengyue@gmail.com>
2022-03-09 15:23:05 +0800

    site: 官网补充图表选中的案例 (#1384)

--- packages/site/examples/column/column/demo/selection.js
+++ packages/site/examples/column/column/demo/selection.js
@@ -1,93 +1,83 @@
-import F2 from '@antv/f2';
+import { Canvas, Chart, Interval, Axis } from '@antv/f2';
 
-const data = [{
-  year: '1951 年',
-  sales: 38
-}, {
-  year: '1952 年',
-  sales: 52
-}, {
-  year: '1956 年',
-  sales: 61
-}, {
-  year: '1957 年',
-  sales: 145
-}, {
-  year: '1958 年',
-  sales: 48
-}, {
-  year: '1959 年',
-  sales: 38
-}, {
-  year: '1960 年',
-  sales: 38
-}, {
-  year: '1962 年',
-  sales: 38
-}];
-const chart = new F2.Chart({
-  id: 'container',
-  pixelRatio: window.devicePixelRatio
-});
-
-chart.source(data, {
-  sales: {
-    tickCount: 5
-  }
-});
-chart.tooltip(false);
-chart.interval().position('year*sales');
-chart.render();
-
-// 绘制柱状图文本
-const offset = -5;
-const canvas = chart.get('canvas');
-const group = canvas.addGroup();
-const shapes = {};
-data.forEach(function(obj) {
-  const point = chart.getPosition(obj);
-  const text = group.addShape('text', {
-    attrs: {
-      x: point.x,
-      y: point.y + offset,
-      text: obj.sales,
-      textAlign: 'center',
-      textBaseline: 'bottom',
-      fill: '#808080'
-    }
-  });
-
-  shapes[obj.year] = text; // 缓存该 shape, 便于后续查找
-});
-
-let lastTextShape; // 上一个被选中的 text
-// 配置柱状图点击交互
-chart.interaction('interval-select', {
-  selectAxisStyle: {
-    fill: '#000',
-    fontWeight: 'bold'
+const data = [
+  {
+    year: '1951 年',
+    sales: 38,
   },
-  mode: 'range',
-  defaultSelected: {
+  {
+    year: '1952 年',
+    sales: 52,
+  },
+  {
+    year: '1956 年',
+    sales: 61,
+  },
+  {
+    year: '1957 年',
+    sales: 145,
+  },
+  {
+    year: '1958 年',
+    sales: 48,
+  },
+  {
+    year: '1959 年',
+    sales: 38,
+  },
+  {
+    year: '1960 年',
+    sales: 38,
+  },
+  {
     year: '1962 年',
-    sales: 38
+    sales: 38,
   },
-  onEnd: function onEnd(ev) {
-    const data = ev.data,
-      selected = ev.selected;
+];
 
-    lastTextShape && lastTextShape.attr({
-      fill: '#808080',
-      fontWeight: 'normal'
-    });
-    if (selected) {
-      const textShape = shapes[data.year];
-      textShape.attr({
-        fill: '#000',
-        fontWeight: 'bold'
-      });
-      lastTextShape = textShape;
-    }
-    canvas.draw();
-  }
-});
+const context = document.getElementById('container').getContext('2d');
+
+const { props } = (
+  <Canvas context={context} pixelRatio={window.devicePixelRatio}>
+    <Chart
+      data={data}
+      scale={{
+        sales: {
+          tickCount: 5,
+        },
+      }}
+    >
+      <Axis field="year" />
+      <Axis field="sales" />
+      <Interval
+        x="year"
+        y="sales"
+        animation={{
+          update: {
+            easing: 'linear',
+            duration: 200,
+            property: ['x', 'y', 'width', 'height'],
+          },
+        }}
+        selection={{
+          selectedStyle: (record) => {
+            const { xMin, xMax } = record;
+            const width = xMax - xMin;
+            const offset = width * 0.2;
+            return {
+              x: xMin - offset,
+              width: width + offset * 2,
+              fillOpacity: 1,
+            };
+          },
+          unSelectedStyle: {
+            fillOpacity: 0.4,
+          },
+        }}
+      />
+    </Chart>
+  </Canvas>
+);
+
+const chart = new Canvas(props);
+chart.render();
