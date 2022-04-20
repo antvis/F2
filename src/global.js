332d2bd77f27398839070b61878ae5377ec9b627
zengyue ye <yezengyue@gmail.com>
2022-04-20 22:40:06 +0800

    chore: release 3.8.11 (#1450)

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@ import lang from './aria-lang.CN';
 import { deepMix } from './util/common';
 
 const Global = {
-  version: '3.8.10',
+  version: '3.8.11',
   scales: {},
   widthRatio: {
     column: 1 / 2,
