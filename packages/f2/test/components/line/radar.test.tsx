46fb275b21dc28f417fce665396e2e2bc0abd2c8
ximenchuixue <mshmayoushi@126.com>
2022-04-28 11:14:19 +0800

    feat(tooltip): 调整snap和辅助线渲染顺序,并使得snap可自动设置填充色 (#1459)

--- packages/f2/test/components/line/radar.test.tsx
+++ packages/f2/test/components/line/radar.test.tsx
@@ -113,6 +113,34 @@ const data1 = [
   },
 ];
 
+const data2 = [
+  ...data1,
+  {
+    time: '10-01',
+    value: 15380,
+    name: '本店',
+  },
+  {
+    time: '12-02',
+    value: 13661,
+    name: '本店',
+  },
+  {
+    time: '12-03',
+    value: 16054,
+    name: '本店',
+  },
+  {
+    time: '12-04',
+    value: 12345,
+    name: '本店',
+  },
+  {
+    time: '12-05',
+    value: 11345,
+    name: '本店',
+  },  
+];
 describe('雷达图', () => {
   describe('面积雷达图', () => {
     it('面积雷达图图', async () => {
@@ -221,5 +249,34 @@ describe('雷达图', () => {
       await delay(1000);
       expect(context).toMatchImageSnapshot();
     });
+
+    it('雷达图交互：选中维度的 snap 点设置不同色', async () => {
+      const context = createContext('雷达图交互：选中维度的 snap 点设置不同色');
+      const { props } = (
+        <Canvas context={context} pixelRatio={1}>
+          <Chart data={data2} coord="polar">
+            <Axis field="time" grid="line" />
+            <Axis field="value" grid="line" style={{ label: null }} />
+            <Line x="time" y="value" color="name" />
+            <Point x="time" y="value" color="name" />
+            <Tooltip custom={true} alwaysShow defaultItem={data2[0]} snap showCrosshairs 
+                // tooltipMarkerStyle中不设置fill时，默认使用record 记录中color自动填充                
+                tooltipMarkerStyle = {{
+                  r: 5,
+                  stroke: '#fff',
+                  lineWidth: '4px',      
+                }}
+            />
+          </Chart>
+        </Canvas>
+      );
+
+      const canvas = new Canvas(props);
+      canvas.render();
+
+      await delay(1000);
+      expect(context).toMatchImageSnapshot();
+    });
+    
   });
 });
