70d7f6feec98152f1fc0e258ec1c705a12be3ce0
zengyue ye <yezengyue@gmail.com>
2022-05-21 00:44:43 +0800

    docs: 添加 react 示例工程 (#1492)

---
+++ packages/react/examples/react/src/index.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
