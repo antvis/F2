a2ce5a4cccde36d7f71c612781673220845c208c
xuying.xu <xuying.xu@alibaba-inc.com>
2022-06-10 16:39:24 +0800

    修复漏斗图 & 金字塔图

--- packages/f2/test/components/interval/example/pyramid.test.tsx
+++ packages/f2/test/components/interval/example/pyramid.test.tsx
@@ -4,7 +4,7 @@ import { Canvas, Chart } from '../../../../src';
 import { Interval, Axis, Legend, Tooltip } from '../../../../src/components';
 import { createContext, delay } from '../../../util';
 
-describe.skip('金字塔图', () => {
+describe('金字塔图', () => {
   it('基础金字塔图', async () => {
     const context = createContext('基础金字塔图');
     const data = [
