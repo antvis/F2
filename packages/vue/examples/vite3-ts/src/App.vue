13a62cfb46d95638ca3785b47cad2d01573557db
KE.YANG <ke.yang@outlook.com>
2022-08-03 22:21:05 +0800

    fix(@antv/f2-vue): lgtm's alerts
    8 for Unused variable, import, function or class

--- packages/vue/examples/vite3-ts/src/App.vue
+++ packages/vue/examples/vite3-ts/src/App.vue
@@ -1,48 +1,3 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { shallowReactive, onMounted } from 'vue';
-import Canvas from '@antv/f2-vue';
-import { Chart, Interval, Axis, Tooltip } from '@antv/f2';
-import Grahpic from './components/Grahpic';
-import Legend from './components/Legend';
-
-const data1 = [
-  { genre: 'Sports', sold: 275 },
-  { genre: 'Strategy', sold: 115 },
-  { genre: 'Action', sold: 120 },
-  { genre: 'Shooter', sold: 350 },
-  { genre: 'Other', sold: 150 },
-];
-
-const data2 = [
-  { genre: 'Sports', sold: 275 },
-  { genre: 'Strategy', sold: 115 },
-  { genre: 'Action', sold: 20 },
-  { genre: 'Shooter', sold: 50 },
-  { genre: 'Other', sold: 50 },
-];
-
-const state = shallowReactive<{
-  data: Record<string, string | number>[];
-  year: number;
-}>({
-  data: data1,
-  year: 2021,
-});
-
-onMounted(() => {
-  setInterval(() => {
-    if (state.year % 2 === 0) {
-      state.year = 2021;
-      state.data = data1;
-    } else {
-      state.year = 2022;
-      state.data = data2;
-    }
-  }, 1000);
-});
-</script>
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -65,7 +20,64 @@ onMounted(() => {
     </Canvas>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, shallowReactive, onMounted, toRefs } from 'vue';
+import Canvas from '@antv/f2-vue';
+import { Chart, Interval, Axis, Tooltip } from '@antv/f2';
+import Grahpic from './components/Grahpic';
+import Legend from './components/Legend';
 
+const data1 = [
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 120 },
+  { genre: 'Shooter', sold: 350 },
+  { genre: 'Other', sold: 150 },
+];
+
+const data2 = [
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 20 },
+  { genre: 'Shooter', sold: 50 },
+  { genre: 'Other', sold: 50 },
+];
+
+export default defineComponent({
+  components: {
+    Canvas,
+    Chart,
+    Interval,
+    Axis,
+    Tooltip,
+    Grahpic,
+    Legend,
+  },
+  setup() {
+    const state = shallowReactive<{
+      data: Record<string, string | number>[];
+      year: number;
+    }>({
+      data: data1,
+      year: 2021,
+    });
+
+    onMounted(() => {
+      setInterval(() => {
+        if (state.year % 2 === 0) {
+          state.year = 2021;
+          state.data = data1;
+        } else {
+          state.year = 2022;
+          state.data = data2;
+        }
+      }, 1000);
+    });
+
+    return { ...toRefs(state) };
+  },
+});
+</script>
 <style scoped>
 .logo {
   height: 6em;
--- packages/vue/examples/vue3-ts/src/App.vue
+++ packages/vue/examples/vue3-ts/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <Canvas>
-      <Chart :data="state.data">
-        <Grahpic :year="state.year" />
+      <Chart :data="data">
+        <Grahpic :year="year" />
         <Legend />
         <Axis field="genre" />
         <Axis field="sold" />
@@ -13,48 +13,63 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import { shallowReactive, onMounted } from "vue"
-import Canvas from "@antv/f2-vue"
-import { Chart, Interval, Axis, Tooltip } from "@antv/f2"
-import Grahpic from "@/components/Grahpic"
-import Legend from "@/components/Legend"
+<script lang="ts">
+import { defineComponent, shallowReactive, onMounted, toRefs } from 'vue';
+import Canvas from '@antv/f2-vue';
+import { Chart, Interval, Axis, Tooltip } from '@antv/f2';
+import Grahpic from '@/components/Grahpic';
+import Legend from '@/components/Legend';
 
 const data1 = [
-  { genre: "Sports", sold: 275 },
-  { genre: "Strategy", sold: 115 },
-  { genre: "Action", sold: 120 },
-  { genre: "Shooter", sold: 350 },
-  { genre: "Other", sold: 150 },
-]
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 120 },
+  { genre: 'Shooter', sold: 350 },
+  { genre: 'Other', sold: 150 },
+];
 
 const data2 = [
-  { genre: "Sports", sold: 275 },
-  { genre: "Strategy", sold: 115 },
-  { genre: "Action", sold: 20 },
-  { genre: "Shooter", sold: 50 },
-  { genre: "Other", sold: 50 },
-]
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 20 },
+  { genre: 'Shooter', sold: 50 },
+  { genre: 'Other', sold: 50 },
+];
 
-const state = shallowReactive<{
-  data: Record<string, string | number>[]
-  year: number
-}>({
-  data: data1,
-  year: 2021,
-})
+export default defineComponent({
+  components: {
+    Canvas,
+    Chart,
+    Interval,
+    Axis,
+    Tooltip,
+    Grahpic,
+    Legend,
+  },
+  setup() {
+    const state = shallowReactive<{
+      data: Record<string, string | number>[];
+      year: number;
+    }>({
+      data: data1,
+      year: 2021,
+    });
 
-onMounted(() => {
-  setInterval(() => {
-    if (state.year % 2 === 0) {
-      state.year = 2021
-      state.data = data1
-    } else {
-      state.year = 2022
-      state.data = data2
-    }
-  }, 1000)
-})
+    onMounted(() => {
+      setInterval(() => {
+        if (state.year % 2 === 0) {
+          state.year = 2021;
+          state.data = data1;
+        } else {
+          state.year = 2022;
+          state.data = data2;
+        }
+      }, 1000);
+    });
+
+    return {...toRefs(state)}
+  },
+});
 </script>
 
 <style>
