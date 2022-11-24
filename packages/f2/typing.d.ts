e9002305907e54de160079c180d4e6f9bccafc9f
索丘 <zengyue.yezy@alibaba-inc.com>
2022-11-24 16:27:01 +0800

    feat: 新增 Gauge 的 ts 类型定义

--- packages/f2/typing.d.ts
+++ packages/f2/typing.d.ts
@@ -1,12 +1,1 @@
 import '../../typings';
-
-// import { JSX as JSXNamespace } from './src/jsx/jsx-namespace';
-
-// // 全局
-// declare global {
-//   namespace JSX {
-//     export type Element = JSXNamespace.Element;
-//     export type ElementClass = JSXNamespace.ElementClass;
-//     export type IntrinsicElements = JSXNamespace.IntrinsicElements;
-//   }
-// }
