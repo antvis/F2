547cd7f04871fed76bf7c063ea17d2a5c482f960
zengyue <yezengyue@gmail.com>
2022-11-29 11:28:47 +0800

    chore: release 3.8.12

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@ import lang from './aria-lang.CN';
 import { deepMix } from './util/common';
 
 const Global = {
-  version: '3.8.11',
+  version: '3.8.12',
   scales: {},
   widthRatio: {
     column: 1 / 2,
