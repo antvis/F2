347027fb2dc98f2e2117dc07a045ca267d30bf8f
Dongjie LIU <patrickliu1989@gmail.com>
2022-12-22 11:04:50 +0800

    feat: tootip 支持多个触发事件 (#1686)
    * feat: tootip 支持多个触发事件
    
    * feat: graphic支持多事件处理
    
    
    Co-authored-by: GIPanda <dongjie.ld@antgroup.com>

--- packages/graphic/src/event/emit.ts
+++ packages/graphic/src/event/emit.ts
@@ -1,6 +1,9 @@
 // 实现简单的事件机制
 import { isObject } from '@antv/util';
 
+// 多个事件分隔符
+const TYPE_SEP = ' ';
+
 class EventEmit {
   // eslint-disable-next-line
   __events: any;
@@ -12,9 +15,11 @@ class EventEmit {
     if (!type || !listener) {
       return;
     }
-    const events = this.__events[type] || [];
-    events.push(listener);
-    this.__events[type] = events;
+    type.split(TYPE_SEP).forEach(item => {
+      const events = this.__events[item] || [];
+      events.push(listener);
+      this.__events[item] = events;
+    });
   }
   emit(type, e) {
     if (isObject(type)) {
@@ -34,22 +39,24 @@ class EventEmit {
   }
   off(type, listener) {
     const __events = this.__events;
-    const events = __events[type];
-    if (!events || !events.length) {
-      return;
-    }
-    // 如果没有指定方法，则删除所有项
-    if (!listener) {
-      delete __events[type];
-      return;
-    }
-    // 删除指定的 listener
-    for (let i = 0, len = events.length; i < len; i++) {
-      if (events[i] === listener) {
-        events.splice(i, 1);
-        i--;
+    type.split(TYPE_SEP).forEach(item => {
+      const events = __events[item];
+      if (!events || !events.length) {
+        return;
       }
-    }
+      // 如果没有指定方法，则删除所有项
+      if (!listener) {
+        delete __events[item];
+        return;
+      }
+      // 删除指定的 listener
+      for (let i = 0, len = events.length; i < len; i++) {
+        if (events[i] === listener) {
+          events.splice(i, 1);
+          i--;
+        }
+      }
+    });
   }
 }
 
