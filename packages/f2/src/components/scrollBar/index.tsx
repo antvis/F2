b8c85c946fd1af4b4e5152cda0c2a4d2d3fa484b
zengyue ye <yezengyue@gmail.com>
2023-06-08 17:09:27 +0800

    fix: 修复手势多次的重复渲染 (#1793)

--- packages/f2/src/components/scrollBar/index.tsx
+++ packages/f2/src/components/scrollBar/index.tsx
@@ -1,6 +1,5 @@
 import withScrollBar, { ScrollBarProps } from './withScrollBar';
 import ScrollBarView from './scrollBarView';
-import withZoom from '../zoom';
 
 export { ScrollBarProps, withScrollBar, ScrollBarView };
-export default withZoom(withScrollBar(ScrollBarView));
+export default withScrollBar(ScrollBarView);
