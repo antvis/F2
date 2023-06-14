3b829b0959a89c0966ccb195a413593ce595dc0f
zengyue ye <yezengyue@gmail.com>
2023-06-14 16:31:50 +0800

    feat: 添加 4.x 5.x benchmark (#1796)

---
+++ site/.dumi/pages/benchmark/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <ol>
+        <li>
+          <a href="./benchmark/4">4.x</a>
+        </li>
+        <li>
+          <a href="./benchmark/5">5.x</a>
+        </li>
+      </ol>
+    </div>
+  );
+};
