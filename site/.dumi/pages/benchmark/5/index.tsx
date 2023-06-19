a6201e931d38727f058451ad1fdca8b709af6716
索丘 <zengyue.yezy@alibaba-inc.com>
2023-06-19 15:18:01 +0800

    site: benchmark 添加 svg 和 webgl

--- site/.dumi/pages/benchmark/5/index.tsx
+++ site/.dumi/pages/benchmark/5/index.tsx
@@ -1,6 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Stats from 'stats.js';
 import { Canvas, Chart, Axis, Line } from '@antv/f2';
+import { Renderer as CanvasRenderer } from '@antv/g-mobile-canvas';
+import { Renderer as SVGRenderer } from '@antv/g-mobile-svg';
+import { Renderer as WebGLRenderer } from '@antv/g-mobile-webgl';
 import data from '../data.json';
 
 // @ts-ignore
@@ -13,20 +16,11 @@ stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 
 document.body.insertBefore(stats.dom, document.body.firstChild);
 
-function renderChart(canvasEl: HTMLCanvasElement) {
-  // 清空画布
-  canvasEl.width = 0;
-  canvasEl.height = 0;
-
+function renderChart(options) {
   function getProps(data) {
     const { props } = (
       // @ts-ignore
-      <Canvas
-        // @ts-ignore
-        context={canvasEl.getContext('2d')}
-        pixelRatio={window.devicePixelRatio}
-        animate={false}
-      >
+      <Canvas pixelRatio={window.devicePixelRatio} animate={false} {...options}>
         <Chart data={data}>
           <Axis field="rate" />
           <Axis field="reportDate" type="timeCat" tickCount={5} />
@@ -43,12 +37,11 @@ function renderChart(canvasEl: HTMLCanvasElement) {
   // @ts-ignore
   const gcanvas = canvas.canvas;
 
-  const len = data.data.length;
-  let i = 0;
+  let flag = true;
   gcanvas.addEventListener('afterrender', () => {
     stats.update();
-    if (i > len) return;
-    canvas.update(getProps(data.data.slice(++i)));
+    flag = !flag;
+    canvas.update(getProps(data.data.slice(flag ? 0 : 1)));
   });
 
   canvas.render();
@@ -57,20 +50,70 @@ function renderChart(canvasEl: HTMLCanvasElement) {
 }
 
 export default () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasInstance, setCanvas] = useState<Canvas | null>(null);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const selectRef = useRef<HTMLSelectElement>(null);
 
   const onRender = () => {
-    const canvasEl = canvasRef.current;
-    if (!canvasEl) return;
+    const container = containerRef.current;
+    if (!container) return;
+    const value = selectRef.current?.value;
+    if (value === 'canvas') {
+      const canvasEl = document.createElement('canvas');
+      canvasEl.style.width = '100%';
+      canvasEl.style.height = '100%';
+      container.appendChild(canvasEl);
+      const canvasRenderer = new CanvasRenderer();
+      const canvas = renderChart({ renderer: canvasRenderer, context: canvasEl.getContext('2d') });
+      setCanvas(canvas);
+      return;
+    }
+    if (value === 'svg') {
+      const svgRenderer = new SVGRenderer();
+      const { width, height } = container.getBoundingClientRect();
+      const canvas = renderChart({ renderer: svgRenderer, container, width, height });
+      setCanvas(canvas);
+      return;
+    }
+    if (value === 'webgl') {
+      const webglRenderer = new WebGLRenderer();
+      const canvasEl = document.createElement('canvas');
+      canvasEl.style.width = '100%';
+      canvasEl.style.height = '100%';
+      container.appendChild(canvasEl);
+      const canvas = renderChart({
+        renderer: webglRenderer,
+        context: canvasEl.getContext('webgl'),
+      });
+      setCanvas(canvas);
+      return;
+    }
+  };
 
-    renderChart(canvasEl);
+  const onSelectChange = () => {
+    try {
+      canvasInstance?.destroy();
+    } catch (e) {}
+    if (containerRef?.current?.firstChild) {
+      containerRef?.current?.removeChild(containerRef?.current?.firstChild as Node);
+    }
+    setCanvas(null);
   };
 
   return (
     <div style={{ paddingTop: '50px' }}>
       <h2>F2 5.x</h2>
-      <canvas ref={canvasRef} style={{ width: '100%', height: '260px' }} />
-      <button onClick={onRender}>start</button>
+      <select ref={selectRef} onChange={onSelectChange}>
+        <option value="canvas">canvas 2d</option>
+        <option value="svg">svg</option>
+        <option value="webgl">webgl</option>
+      </select>
+      <div ref={containerRef} style={{ width: '100%', height: '260px' }}></div>
+      <button onClick={onRender} style={{ marginRight: '10px' }}>
+        render
+      </button>
+      <button style={{ marginRight: '10px' }}>pinch</button>
     </div>
   );
 };
