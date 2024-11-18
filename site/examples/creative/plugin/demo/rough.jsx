cb5c2df312366a45fcc13a13baf8be35a85f2073
xuying.xu <xuying.xu@alibaba-inc.com>
2024-11-18 15:57:43 +0800

    chore: 增加player组件例子

--- site/examples/creative/plugin/demo/rough.jsx
+++ site/examples/creative/plugin/demo/rough.jsx
@@ -1,14 +1,25 @@
 /** @jsx jsx */
-import { Canvas, Chart, Interval, jsx, Axis, CanvasRenderer } from '@antv/f2';
+import { Axis, Canvas, CanvasRenderer, Chart, Interval, jsx } from '@antv/f2';
 import { Plugin as PluginRoughCanvasRenderer } from '@antv/g-plugin-rough-canvas-renderer';
 const context = document.getElementById('container').getContext('2d');
 
 const data = [
-    { genre: 'Sports', sold: 275 },
-    { genre: 'Strategy', sold: 115 },
-    { genre: 'Action', sold: 120 },
-    { genre: 'Shooter', sold: 350 },
-    { genre: 'Other', sold: 150 },
+  { version: '5.0.21', rate: 1, type: 'iphone6' },
+  { version: '5.0.21', rate: 13, type: '仿真' },
+  { version: '5.0.21', type: 'iphone8', rate: 11 },
+  { version: '5.0.21', type: 'iphone x', rate: 0 },
+
+  { version: '5.0.29', rate: 1, type: 'iphone6' },
+  { version: '5.0.29', rate: 14, type: '仿真' },
+  { version: '5.0.29', type: 'iphone8', rate: 10 },
+  { version: '5.0.29', type: 'iphone x', rate: 0 },
+
+  { version: '5.0.30', rate: 1, type: 'iphone6' },
+  { version: '5.0.30', rate: 15, type: '仿真' },
+  { version: '5.0.30', type: 'iphone8', rate: 13 },
+  { version: '5.0.30', type: 'iphone x', rate: 12 },
+
+  { version: 'next', type: 'iphone x', rate: 24 },
 ];
 
 // create a renderer
@@ -18,9 +29,9 @@ canvasRenderer.registerPlugin(new PluginRoughCanvasRenderer());
 const { props } = (
   <Canvas context={context} pixelRatio={window.devicePixelRatio} renderer={canvasRenderer}>
     <Chart data={data}>
-      <Axis field="genre" />
-      <Axis field="sold" />
-      <Interval x="genre" y="sold" color="genre"/>
+      <Axis field="version" />
+      <Axis field="rate" />
+      <Interval x="version" y="rate" color="version" />
     </Chart>
   </Canvas>
 );
