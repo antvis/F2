1fa7bc032f75f82f982b33f01c2aa87f55d2c597
xuying <33517362+tangying1027@users.noreply.github.com>
2025-06-23 20:05:31 +0800

    feat: 优化饼图显示 (#2071)
    * feat: 优化饼图显示
    
    * feat: 优化二段式绘制
    
    * feat: 增加两段式type类型
    
    * chore: ts error
    
    ---------
    
    Co-authored-by: xuying.xu <xuying.xu@alibaba-inc.com>

--- packages/f2/src/components/pieLabel/withPieLabel.tsx
+++ packages/f2/src/components/pieLabel/withPieLabel.tsx
@@ -1,7 +1,7 @@
 import { jsx, Component } from '@antv/f-engine';
 import { deepMix, isFunction } from '@antv/util';
 import { ChartChildProps, Point } from '../../chart';
-
+import { adjustPosition } from './spider';
 const DEFAULT_CONFIG = {
   anchorOffset: '10px', // 锚点的偏移量
   inflectionOffset: '30px', // 拐点的偏移量
@@ -17,6 +17,8 @@ const DEFAULT_CONFIG = {
   // },
   label1OffsetY: '-4px',
   label2OffsetY: '4px',
+  type: 'default', // 标签布局类型：default 或 spider
+  adjustRatio: 1 / 2, // 调整高度的阈值比例
 };
 
 function getEndPoint(center: Point, angle: number, r: number) {
@@ -59,7 +61,6 @@ function isThirdQuadrant(angle: number) {
 function isFourthQuadrant(angle: number) {
   return angle >= Math.PI && angle < (Math.PI * 3) / 2;
 }
-
 export interface PieLabelProps {
   anchorOffset?: string | number;
   inflectionOffset?: string | number;
@@ -71,6 +72,16 @@ export interface PieLabelProps {
    */
   triggerOn?: 'click' | 'press';
   onClick?: (ev) => void;
+  /**
+   * 调整高度的阈值比例，用于判断是否使用两段式连线
+   * @default 0.5
+   */
+  adjustRatio?: number;
+  /**
+   * 布局类型
+   *
+   */
+  type?: 'default' | 'spider';
 }
 
 export default (View) => {
@@ -293,10 +304,133 @@ export default (View) => {
       return labels;
     }
 
+    getSpiderLabels = (props) => {
+      const {
+        chart,
+        coord,
+        anchorOffset,
+        inflectionOffset,
+        label1,
+        label2,
+        height: itemHeight,
+        sidePadding,
+      } = props;
+      const { measureText } = this.context;
+      const { center, radius, height: coordHeight, width: coordWidth } = coord;
+
+      const maxCountForOneSide = Math.floor(coordHeight / itemHeight);
+      const maxCount = maxCountForOneSide * 2;
+
+      const geometry = chart.getGeometrys()[0];
+      const records = geometry
+        .flatRecords()
+        // 按角度大到小排序
+        .sort((a, b) => {
+          const angle1 = a.xMax - a.xMin;
+          const angle2 = b.xMax - b.xMin;
+          return angle2 - angle1;
+        })
+        // 只取前 maxCount 个显示
+        .slice(0, maxCount);
+
+      // 存储左右 labels
+      let halves = [
+        [], // left
+        [], // right
+      ];
+      // label 的最大宽度
+      const labelWidth =
+        coordWidth / 2 - radius - anchorOffset - inflectionOffset - 2 * sidePadding;
+      records.forEach((record) => {
+        const { xMin, xMax, color, origin } = record;
+
+        // 锚点角度
+        const anchorAngle = getMiddleAngle(xMin, xMax);
+        // 锚点坐标
+        const anchorPoint = getEndPoint(center, anchorAngle, radius + anchorOffset);
+        // 拐点坐标
+        const inflectionPoint = getEndPoint(center, anchorAngle, radius + inflectionOffset);
+        // 锚点方向
+        const side = anchorPoint.x < center.x ? 'left' : 'right';
+
+        const label = {
+          origin,
+          angle: anchorAngle,
+          anchor: anchorPoint,
+          inflection: inflectionPoint,
+          side,
+          x: inflectionPoint.x,
+          y: inflectionPoint.y,
+          r: radius + inflectionOffset,
+          color,
+          label1: isFunction(label1) ? label1(origin, record) : label1,
+          label2: isFunction(label2) ? label2(origin, record) : label2,
+          height: 0,
+        };
+
+        const height =
+          measureText(label.label1.text, label.label1).height +
+          measureText(label.label2.text, label.label2).height;
+        label.height = height;
+
+        // 判断文本的方向
+        if (side === 'left') {
+          halves[0].push(label);
+        } else {
+          halves[1].push(label);
+        }
+      });
+
+      // 判断是有一边超过了显示的最大
+      if (halves[0].length > maxCountForOneSide) {
+        halves = move(halves[0], halves[1], halves[0].length - maxCountForOneSide, center);
+      } else if (halves[1].length > maxCountForOneSide) {
+        const [right, left] = move(
+          halves[1],
+          halves[0],
+          halves[1].length - maxCountForOneSide,
+          center
+        );
+        halves = [left, right];
+      }
+
+      let labels = [];
+      halves.forEach((half, index) => {
+        const showSide = index === 0 ? 'left' : 'right';
+
+        // 顺时针方向排序
+        half.sort((a, b) => {
+          let aAngle = a.angle;
+          let bAngle = b.angle;
+          if (showSide === 'left') {
+            // 是否在第一象限
+            aAngle = isFirstQuadrant(aAngle) ? aAngle + Math.PI * 2 : aAngle;
+            bAngle = isFirstQuadrant(bAngle) ? bAngle + Math.PI * 2 : bAngle;
+            return bAngle - aAngle;
+          } else {
+            // 是否在第四象限
+            aAngle = isFourthQuadrant(aAngle) ? aAngle - Math.PI * 2 : aAngle;
+            bAngle = isFourthQuadrant(bAngle) ? bAngle - Math.PI * 2 : bAngle;
+            return aAngle - bAngle;
+          }
+        });
+
+        labels = labels.concat(adjustPosition(half, showSide, props, labelWidth));
+      });
+
+      return labels;
+    };
+
     render() {
       const { context } = this;
       const props = context.px2hd(deepMix({}, DEFAULT_CONFIG, this.props));
-      const labels = this.getLabels(props);
+      const { type } = props;
+      let labels = [];
+      if (type === 'default') {
+        labels = this.getLabels(props);
+      } else if (type === 'spider') {
+        labels = this.getSpiderLabels(props);
+      }
       return <View labels={labels} {...props} />;
     }
   };
