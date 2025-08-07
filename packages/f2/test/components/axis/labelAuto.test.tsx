f37cbdb3f19a00225489669253001532c1b96eed
tangying1027 <370064808@qq.com>
2025-08-07 20:11:47 +0800

    chore: 修复单测

--- packages/f2/test/components/axis/labelAuto.test.tsx
+++ packages/f2/test/components/axis/labelAuto.test.tsx
@@ -79,7 +79,7 @@ describe('Axis labelAutoRotate', () => {
     const canvas = new Canvas(props);
     await canvas.render();
 
-    await delay(500);
+    await delay(1000);
     expect(context).toMatchImageSnapshot();
   });
 
@@ -109,7 +109,7 @@ describe('Axis labelAutoRotate', () => {
     const canvas = new Canvas(props);
     await canvas.render();
 
-    await delay(800);
+    await delay(1000);
     expect(context).toMatchImageSnapshot();
   });
 
@@ -139,7 +139,7 @@ describe('Axis labelAutoRotate', () => {
     const canvas = new Canvas(props);
     await canvas.render();
 
-    await delay(800);
+    await delay(1000);
     expect(context).toMatchImageSnapshot();
   });
 
@@ -169,7 +169,7 @@ describe('Axis labelAutoRotate', () => {
     const canvas = new Canvas(props);
     await canvas.render();
 
-    await delay(800);
+    await delay(1000);
     expect(context).toMatchImageSnapshot();
   });
 });
