0b521963e992a7c970b36d8bcbb7f0a7967c2ddc
tangying1027 <370064808@qq.com>
2025-10-01 15:55:13 +0800

    feat: legend增加adaptive属性

--- packages/f2/src/components/legend/withLegend.tsx
+++ packages/f2/src/components/legend/withLegend.tsx
@@ -215,6 +215,66 @@ export default (View) => {
       this.legendStyle = style;
     }
 
+    _adaptiveLayout(node) {
+      const { props, context } = this;
+      const {
+        // @ts-ignore
+        layout: parentLayout,
+        width: customWidth,
+        height: customHeight,
+        position = 'top',
+      } = props;
+      const items = this.getItems();
+      if (!items || !items.length) return;
+      const { left, top, width: layoutWidth, height: layoutHeight } = parentLayout;
+      const width = context.px2hd(customWidth) || layoutWidth;
+
+      const { width: itemMaxWidth, height: itemMaxHeight } = this.getMaxItemBox(node);
+
+      const lineCount = 1; // 先不考虑多行的情况
+
+      const autoHeight = itemMaxHeight * lineCount;
+      const style: GroupStyleProps = {
+        left,
+        top,
+        width,
+        // height 默认自适应
+        height: undefined,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      };
+      // 如果只有一行，2端对齐
+      if (lineCount === 1) {
+        style.justifyContent = 'space-between';
+      }
+      if (position === 'top') {
+        style.height = customHeight ? customHeight : autoHeight;
+      }
+      if (position === 'left') {
+        style.flexDirection = 'column';
+        style.justifyContent = 'center';
+        style.alignItems = 'flex-start';
+        style.width = itemMaxWidth;
+        style.height = customHeight ? customHeight : layoutHeight;
+      }
+      if (position === 'right') {
+        style.flexDirection = 'column';
+        style.alignItems = 'flex-start';
+        style.justifyContent = 'center';
+        style.left = left + (width - itemMaxWidth);
+        style.width = itemMaxWidth;
+        style.height = customHeight ? customHeight : layoutHeight;
+      }
+      if (position === 'bottom') {
+        style.top = top + (layoutHeight - autoHeight);
+        style.height = customHeight ? customHeight : autoHeight;
+      }
+
+      this.legendStyle = style;
+    }
+
     updateCoord() {
       const { context, props, legendStyle } = this;
       const { position = 'top', margin = '30px', chart } = props;
